775f5b86f307a77295012006caf148473d086009
João Guilherme de Sá <jgsdsa@restinga.ifrs.edu.br>
2018-08-28 19:36:40 -0300

    commit inicial do projeto

---
+++ src/main/java/br/com/markethub/markethub/MarkethubApplication.java
@@ -0,0 +1,12 @@
+package br.com.markethub.markethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkethubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkethubApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/markethub/markethub/MarkethubApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.markethub.markethub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarkethubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
